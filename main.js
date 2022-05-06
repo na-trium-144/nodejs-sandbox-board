333877213be59a389dc5d624bca6f3205505dc51
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2022-05-07 00:03:48 +0900

    SQLにコメントを保存できるようになった

--- main.js
+++ main.js
@@ -8,59 +8,84 @@ const app = express();
 // app.use(express.static("static"));
 app.use(express.urlencoded({ extended: true }));
 
-let comments = JSON.parse(fs.readFileSync("comments.json", "utf-8"));
+const { PrismaClient } = require("@prisma/client");
+const client = new PrismaClient();
 
-app.post("/delete", (request, response) => {
-    comments = comments.filter((m) => (m.id !== parseInt(request.body.id, 10)));
-    fs.writeFileSync("comments.json", JSON.stringify(comments), "utf-8");
+// const comments = client.comment.findMany();
+
+app.post("/delete", async (request, response) => {
+    await client.comment.delete({
+        where:{
+            id:parseInt(request.body.id, 10)
+        }
+    });
+    // comments = comments.filter((m) => (m.id !== parseInt(request.body.id, 10)));
+    // fs.writeFileSync("comments.json", JSON.stringify(comments), "utf-8");
     response.send(ejs.render(fs.readFileSync("message.ejs", "utf-8"), { text:"コメントを削除しました。" }));
 });
 
-app.post("/send", (request, response) => {
+app.post("/send", async (request, response) => {
     message = {
         name: request.body.name,
-        age:request.body.age,
+        content:request.body.content,
         edited:false,
-        time:new Date().getTime(),
-        id:parseInt(request.body.id, 10)
+        time:new Date(),
+        // id:parseInt(request.body.id, 10)
     };
-    if(request.body.id === undefined){
-        if(comments.length === 0){
-            message.id = 0
-        }else{
-            message.id = comments[comments.length - 1].id + 1;
+    // if(request.body.id === undefined){
+        // if(comments.length === 0){
+        //     message.id = 0
+        // }else{
+        //     message.id = comments[comments.length - 1].id + 1;
+        // }
+        // comments.push(message);
+    await client.comment.create({data:message});
+    response.send(ejs.render(fs.readFileSync("message.ejs", "utf-8"), { text:"コメントを送信しました。" }));
+    // }
+});
+app.post("/send_edit", async (request, response) => {
+    // else{
+    await client.comment.update({
+        data:{
+            name:request.body.name,
+            content:request.body.content,
+            edited:true
+        },
+        where:{
+            id:parseInt(request.body.id, 10)
         }
-        // if(message.name === ""){
-            //     message.name = "名無し";
-            // }
-        comments.push(message);
-        response.send(ejs.render(fs.readFileSync("message.ejs", "utf-8"), { text:"コメントを送信しました。" }));
-    }else{
-        message.edited = true;
-        const editTargetIndex = comments.findIndex((m) => (m.id === parseInt(request.body.id, 10)));
-        if(editTargetIndex === -1){
-            response.send(ejs.render(fs.readFileSync("message.ejs", "utf-8"), { text:"コメントが存在しません。" }));
-        }else{
-            comments[editTargetIndex] = message;
-            response.send(ejs.render(fs.readFileSync("message.ejs", "utf-8"), { text:"コメントを編集しました。" }));
-        }
-    }
-    fs.writeFileSync("comments.json", JSON.stringify(comments), "utf-8");
+    });
+        // message.edited = true;
+        // const editTargetIndex = comments.findIndex((m) => (m.id === parseInt(request.body.id, 10)));
+        // if(editTargetIndex === -1){
+        //     response.send(ejs.render(fs.readFileSync("message.ejs", "utf-8"), { text:"コメントが存在しません。" }));
+        // }else{
+        //     comments[editTargetIndex] = message;
+    response.send(ejs.render(fs.readFileSync("message.ejs", "utf-8"), { text:"コメントを編集しました。" }));
+        // }
+    // }
+    // fs.writeFileSync("comments.json", JSON.stringify(comments), "utf-8");
 });
 
-app.get("/", (request, response) => {
+app.get("/", async (request, response) => {
+  const comments = await client.comment.findMany();
   const template = fs.readFileSync("template.ejs", "utf-8");
   const html = ejs.render(template, { books: comments, format: dateFns.format });
   response.send(html);
 });
-app.get("/edit", (request, response) => {
-    const editTargetIndex = comments.findIndex((m) => (m.id === parseInt(request.query.id, 10)));
-    if(editTargetIndex === -1){
-        response.send(ejs.render(fs.readFileSync("message.ejs", "utf-8"), { text:"コメントが存在しません。" }));
-    }else{
-        const template = fs.readFileSync("edit.ejs", "utf-8");
-        const html = ejs.render(template, { book:comments[editTargetIndex] });
-        response.send(html);
-    }
+app.get("/edit", async (request, response) => {
+    const comment = await client.comment.findUnique({
+        where:{
+            id:parseInt(request.query.id, 10)
+        }
+    });
+    // const editTargetIndex = comments.findIndex((m) => (m.id === parseInt(request.query.id, 10)));
+    // if(editTargetIndex === -1){
+    //     response.send(ejs.render(fs.readFileSync("message.ejs", "utf-8"), { text:"コメントが存在しません。" }));
+    // }else{
+    const template = fs.readFileSync("edit.ejs", "utf-8");
+    const html = ejs.render(template, { book:comment });
+    response.send(html);
+    // }
 });
 app.listen(3000);
